e72f4c3ea84727f7a3a4301aa31b97061466bd27
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2024-01-21 16:13:46 -0600

    new

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
+using Dapper;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();
+
+            return View(products);
+        }
+
+
+    }
+
+}
+
+    
+
